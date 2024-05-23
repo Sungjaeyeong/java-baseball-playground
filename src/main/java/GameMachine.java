edaddc371dd4931775e9cbd1db17e5ca3a7f0332
Sungjaeyeong <wodud2587@gmail.com>
2024-05-23 23:32:14 +0900

    feat: 서로 다른 3자리 수 생성

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -1,8 +1,26 @@
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class GameMachine {
      int oneToNineRandomNumber() {
         Random random = new Random();
         return 1 + random.nextInt(9);
+    }
+
+    int[] generateUniqueThreeDigitArray() {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < 3) {
+            uniqueNumbers.add(oneToNineRandomNumber());
+        }
+
+        int[] result = new int[3];
+        int i = 0;
+        for (int number : uniqueNumbers) {
+            result[i++] = number;
+        }
+
+        return result;
     }
 }
