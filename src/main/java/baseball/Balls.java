cb57b55dfd5e560feb74dda16655466ff88c5917
Sungjaeyeong <wodud2587@gmail.com>
2024-05-29 00:28:15 +0900

    feat: Balls 필드 변경

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -5,17 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Balls {
-    Ball firstBall;
-    Ball secondBall;
-    Ball thirdBall;
+    List<Ball> ballList;
 
     public Balls(int no1, int no2, int no3) {
         if (no1 == no2 || no1 == no3 || no2 == no3) throw new IllegalArgumentException("각 번호는 서로 같을 수 없습니다.");
-        this.firstBall = new Ball(no1, BallOrder.FIRST);
-        this.secondBall = new Ball(no2, BallOrder.FIRST);
-        this.thirdBall = new Ball(no3, BallOrder.FIRST);
-    }
 
+        this.ballList = Arrays.asList(
+                new Ball(no1, BallOrder.FIRST),
+                new Ball(no2, BallOrder.SECOND),
+                new Ball(no3, BallOrder.THIRD)
+        );
+
+    }
 
     public boolean isStrike(Ball ball) {
         return true;
